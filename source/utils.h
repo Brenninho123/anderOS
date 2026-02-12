f22fa66f1df86a3ce12c503ad77e671d9c653f3d
Ander <157061485+Brenninho123@users.noreply.github.com>
2026-02-12 13:31:31 -0300

    Add header file for utility functions

---
+++ source/utils.h
@@ -0,0 +1,10 @@
+#ifndef UTILS_H
+#define UTILS_H
+
+void print(const char* str, int color);
+void print_char(char c, int color, int row, int col);
+void clear_screen();
+void scroll();
+int strcmp(const char* s1, const char* s2);
+
+#endif
