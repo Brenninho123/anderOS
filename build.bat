a937019d5949507771e61ba40a4527165baea00b
Ander <157061485+Brenninho123@users.noreply.github.com>
2026-02-12 13:35:06 -0300

    Atualizar o build.bat

--- build.bat
+++ build.bat
@@ -1,12 +1,17 @@
 @echo off
 echo Building anderOS...
 
-REM Compile kernel
-gcc -ffreestanding -m32 -c kernel.c -o kernel.o
+REM Compile all source files
+gcc -ffreestanding -m32 -c source\utils.c -o utils.o
+gcc -ffreestanding -m32 -c source\keyboard.c -o keyboard.o
+gcc -ffreestanding -m32 -c source\logo.c -o logo.o
+gcc -ffreestanding -m32 -c source\terminal.c -o terminal.o
+gcc -ffreestanding -m32 -c source\kernel.c -o kernel.o
+
 if errorlevel 1 goto error
 
 REM Link kernel
-ld -m elf_i386 -T linker.ld -o kernel.bin --oformat binary kernel.o
+ld -m elf_i386 -T linker.ld -o kernel.bin --oformat binary kernel.o utils.o keyboard.o logo.o terminal.o
 if errorlevel 1 goto error
 
 REM Assemble bootloader
