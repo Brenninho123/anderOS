8b63e0cf63c26a6718c9c2d319d54328caf0e56c
Ander <157061485+Brenninho123@users.noreply.github.com>
2026-02-12 15:49:02 -0300

    Atualizar o build.bat

--- build.bat
+++ build.bat
@@ -3,24 +3,27 @@ echo ===============================
 echo Building anderOS 32-bit Protected Mode
 echo ===============================
 
-REM --- Compile todos os módulos 32-bit ---
+REM --- Compile all modules ---
 gcc -ffreestanding -m32 -c source\utils.c -o utils.o
 gcc -ffreestanding -m32 -c source\keyboard.c -o keyboard.o
 gcc -ffreestanding -m32 -c source\logo.c -o logo.o
 gcc -ffreestanding -m32 -c source\terminal.c -o terminal.o
 gcc -ffreestanding -m32 -c source\kernel.c -o kernel.o
+gcc -ffreestanding -m32 -c source\os\system.c -o system.o
+gcc -ffreestanding -m32 -c source\os\memory.c -o memory.o
+gcc -ffreestanding -m32 -c source\os\io.c -o io.o
 
 if errorlevel 1 goto error
 
 REM --- Link kernel ---
-ld -m elf_i386 -T linker.ld -o kernel.bin --oformat binary kernel.o utils.o keyboard.o logo.o terminal.o
+ld -m elf_i386 -T linker.ld -o kernel.bin --oformat binary kernel.o utils.o keyboard.o logo.o terminal.o system.o memory.o io.o
 if errorlevel 1 goto error
 
 REM --- Assemble bootloader ---
 nasm -f bin boot.asm -o boot.bin
 if errorlevel 1 goto error
 
-REM --- Cria imagem do OS ---
+REM --- Create OS image ---
 copy /b boot.bin + kernel.bin anderOS.bin > nul
 
 echo.
@@ -28,7 +31,6 @@ echo Build successful!
 echo Running in QEMU...
 echo.
 
-REM --- Roda no QEMU ---
 qemu-system-x86_64 -m 64M -drive format=raw,file=anderOS.bin -boot a
 
 goto end
